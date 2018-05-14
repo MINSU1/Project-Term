4efbd5060f336ed23b98add4a5dd9068e5987ad4
rnorfield <rnorfield@gmail.com>
2018-05-14 00:04:31 -0700

    Jakob (#43)
    Jakob > Login, server, DB

--- unittest/signup.test.js
+++ unittest/signup.test.js
@@ -1,4 +1,12 @@
-// import { mount } from 'enzyme';
-// const wrapper = mount(<MyComponent />)
-// const regester = require('../public/register.js')
+var div1 = document.createElement('button')
+div1.id = 'submit'
+
+//const regester = require('../public/register.js')
+
+
+describe.skip('Test the root path', () => {
+    test('/ path should response the GET method', (done) => {
+        expect(regester.errorTest()).toBe(false);
+    });
+})
 
