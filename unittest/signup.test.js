06b20f3c0e781823fda55f11c399a453c06a5316
rnorfield <rnorfield@gmail.com>
2018-05-15 15:53:23 -0700

    Dev (#52)
    Dev into master

--- unittest/signup.test.js
+++ unittest/signup.test.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var div1 = document.createElement('button')
 div1.id = 'submit'
 
@@ -10,9 +9,4 @@ describe.skip('Test the root path', () => {
         expect(regester.errorTest()).toBe(false);
     });
 })
-=======
-// import { mount } from 'enzyme';
-// const wrapper = mount(<MyComponent />)
-// const regester = require('../public/register.js')
->>>>>>> 0ea407d2faa1f211ac5863b532387af15d094ec7
 
