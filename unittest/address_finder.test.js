004ab106129305dfede20cf91bd6894a845c670a
Jay Kim <yeonjae.jay.kim@gmail.com>
2018-05-18 15:34:37 -0700

    app.js revised

--- unittest/address_finder.test.js
+++ unittest/address_finder.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../address_finder')
+const app = require('../address_finder.js')
 
 describe('Test getWeather', () => {
     test('getWeather should return summary, temperature, pressure', () => {
