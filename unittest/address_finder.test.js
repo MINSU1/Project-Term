e3ad7050d9458a01a5dc328b5febd85663b71358
Jay Kim <yeonjae.jay.kim@gmail.com>
2018-05-18 15:14:42 -0700

    unittesting update and package has been changed

--- unittest/address_finder.test.js
+++ unittest/address_finder.test.js
@@ -1,5 +1,5 @@
 const request = require('supertest');
-const app = require('../address_finder.js')
+const app = require('../address_finder')
 
 describe('Test getWeather', () => {
     test('getWeather should return summary, temperature, pressure', () => {
